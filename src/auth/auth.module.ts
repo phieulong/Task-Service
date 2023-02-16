7c17d34f9b1c410ce329c9725c6a30634c758e3d
nhatnq3 <nhatnq3@onemount.com>
2023-02-16 16:15:41 +0700

    hehe

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,14 +11,9 @@ import { JwtStrategy } from './jwt.strategy';
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      privateKey: {
-        key: 'MIIEpAIBAAKCAQEAxjNvuEx2crcO71BBt1TpVX8O+fz62FahxL9jxdb1qsxGR50K8C2EvFbdqEJEYewDSSEiZhiPNviEvmCq5n3t+0RikadYKEv56jTjPSg9Zqe3W9x5z9c6Bm71wWz4XiseUuhXuOU0fV+rRqSxsjMd4Zx5uS1DGOVdIIKx5ybURR3nbbOK0yyH4qg8hhYYNWZpMSp4+LvDF8b+iIDWzx5wnNYySd5UNRdKxj8/AtSglY6KSkvg6+zEEF3KFQEvXbWmzlGW4Z2elVjahqt7VRpjy2mmXo7THM5NvwJRGWWzPwUSh3loyheXbZWjG+6IiHRRFBrN2w83vZUH1OoBBRcRNwIDAQABAoIBABbCtWOutDN8PiTRTJqnD5yX2LpwiMXrPOyBJg4EH3Ybuz5iFG18bK/MMewun/e2yLHPMkoTK+8eHvBV3YpWj8u5ELC1TPRjtTFmHWpagqKVRAMykHKDvtAaUfKCdwspXHQc5mX/PEXhhKO6KgXSxO2xsHDJd9kWDdm76H4pAVtmXflpOkZpHNRqeBQQuPuV+pwOJAeuw0Zr4Hsi4yBS1hDDhZlP4JpKGtb3NlSjsUU1gXAd88UrH+6CE2mucImOpYFwBm4rF3KOshzp4MKauLrtpwSLb32uyGuvIEBF6QXP/6EMpcp4RjLulvpigYcbO+9NWlVcz8yKY9yzRmVPlgECgYEA5+lRS6+BEaNCnbSb/BdsnfyLzNVKIJ/84X369lLjBFGF6yVx+nGRiaKB7WbwaZukHGOWkkZ+RgWuDlmFmeFxshEQL8KDzaXVs5c0aygirXKbOWrmyCyoTnlPBIjFWEDZYYNvIHnj0mdLl57LLrTI9kVcHHKOCw6PnqwQ/hOOUrcCgYEA2sm7/W8Rmh2tcbIWaLWVcRBEDW204nHU5k9S2EfIGPhUe8UP1OTVYVDXmkVa/+icCSkPHJr+EWi0bh4QfveiExQCwDw7+w8Bdmig2F+8euZdkHUJHxruNVdEjA7i2HN9Nh3Qfuduusdm5JwtfY5jV7pcIPRWRo6zN0GBFBE8tYECgYEA1YHIhpWdjk9wFSEiOoSWSx0m1/3FsSOZ/T+uvPiyaFdlo4dl19yW0M/8LkSHMbNI85n8Fc8wkebIyA1DItibhASRkmbSUZJLnHuolr7sriLFJad1sPTsPtMQHGyOwyFxAIWw2YVeaWcQARcAmI94NMLGeqo8e5F8R+jB/Cik7yECgYEAzoJp6d4DbEek5dMlqbHzm6OxyEcL3E17meO3m6OtcDw9ahHoTper/M4Uo23hZfmGBnN16xc0xvPgkqvYAC/3EDDsI8z3BJL2W3CImZNL0WlNWUPO10hRHZIAhn6qiYoA3qqnmJEUCEKckR4FJiTXCgBCY2ydXr3k1TwD/RAEEIECgYA/S/yjas2IFKxb+j3TdP8RxlUebvopB0Oa+D0BcFOoYx6CVFUFcgq4BZp/zboocpBOPOA18WBq5q1mwOtDwX9S1Iwp0shxPbfljZ+/16U95wzes/X3k6c4m8MdhvjasuFfrsVUemIrDe+PdhK1ikN1N/rLcU1+8CW7zf7SjNEGcg==',
-        passphrase: '',
-        type: 'private',
-      },
+      secretOrPrivateKey: 'secret',
       signOptions: {
         expiresIn: 3600,
-        algorithm: 'RS256',
       },
     }),
     TypeOrmModule.forFeature([User]),
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -13,10 +13,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: Repository<User>,
   ) {
     super({
-      secretOrKey:
-        'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2lipj3v81B/tiL9kZ+7US6MSjKgLyIh21v4Qa3sErlGuT5KHdbqP+x8vqWDJK31O1jBQpBfEinT+RjT31se1UgueDwp51ufdC5rhVHl6A6/NlPwjy0JqbuRik/hiPrtArASMdXoCTb34JhwzOS9RMmNRHAKATNiZQw7DRqnWCe1K2J+yM2xAFplbvcophFY4i5CzF+pvCkYsZcKHcLjy5vYvEka/3ocvBl2RX6JDBhWAP+TpZxWfV2hoEGxersGYJLlCXygeWHfiHd6QPAeoGu6/mj1KVKrQUTqkRivtn8kjnHqX03SV0CvI7HslOe2ZozNDoWxuyq2X1Z0/FEDEgQIDAQAB',
+      secretOrKey: 'secret',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      algorithms: ['RS256'],
     });
   }
 
