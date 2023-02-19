4a782606c17468d370a7fc74577cbb74465c3cc5
nhatnq3 <nhatnq3@onemount.com>
2023-02-19 17:57:20 +0700

    hehe

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,21 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private configService: ConfigService,
   ) {
     super({
-      secretOrKey:
-        '-----BEGIN PUBLIC KEY-----\n' +
-        'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAguYYbnMA82LkDw1JOnoq\n' +
-        'QNNCHX8I6LL7CDIkLZ5JvePBmtenX5K92uHijaHUySR/rSLe5/S/QBnEPSLSQN7V\n' +
-        'lGTK7085qQofCVZxnLZ3paME9cufIhihgzhsRj9tfk9txq0WfWNAnSXWab/mTFqF\n' +
-        '6188E3DedlfFTC3Obca6i4iPMbRUIKrH1FDDedcG3UjWeoB5jKSqlnEaRC204Aew\n' +
-        '/1h+ye/JfE0brFQmmjzbs0S+PsSlu0/CaOqWFC6DdWYAHbqxuyBd0kIFinwE2eJY\n' +
-        '7Lk/1qKGE59Uevn1cfg6npIbFAjSyVc3legAUoQ9Nb1ginLf0NW2X+89jMVaMMtq\n' +
-        'dAbnUwcEPbkcTyIkMqOFGfOmMuRTklBIVwDctje7i69GmQFrrdEa7Q5iteqd5x/5\n' +
-        '74XtxSIDmqJyk1YKJ8r3Wz8r7wHLTYO4Ygr9RTxbfaaljC6jSnSErzqZlIq2FOVg\n' +
-        'jJgXML78N0XOiCDSXysBMnoBUEkz5rqAZAMNDECZP+VJAp25xRkF0nFgeVdKCgrQ\n' +
-        'DztZTFyhaAGgKlro2jFmwr4iWxrZkfk2fB2iKxSdXnuIaaBD57LV59sVemC3MVkN\n' +
-        'WErKb0gqfs5y/GZ9fxopLohTCZar46F9SvWYAUV4z/PWMBi81UMz97ZSlQVMfnHZ\n' +
-        'zbfc5hs8ZO2WT+OGyo+keWsCAwEAAQ==\n' +
-        '-----END PUBLIC KEY-----',
+      secretOrKey: configService.get('RSA_PUBLIC_KEY').replace(/\\n/g, '\n'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       algorithms: ['RS256'],
     });
