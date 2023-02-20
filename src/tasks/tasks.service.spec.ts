7402d0567bf0c9841cd9b365ff4d60f1111f2c4b
nhatnq3 <nhatnq3@onemount.com>
2023-02-20 12:21:10 +0700

    hehe

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -4,6 +4,7 @@ import { TasksService } from './tasks.service';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 export type MockType<T> = { [P in keyof T]?: jest.Mock };
 
@@ -28,22 +29,39 @@ describe('TasksService', () => {
     service = module.get<TasksService>(TasksService);
     repositoryMock = module.get(getRepositoryToken(Task));
   });
+
   describe('getById', () => {
-    it('calls TasksService.getById should return the result ', async () => {
-      const user = { id: '123', username: 'Alni', password: '123', tasks: [] };
-      const task = { id: '123' };
+    it('RETURN the result', async () => {
+      const user = {
+        id: randomUUID(),
+        username: 'username',
+        password: 'password',
+        tasks: [],
+      };
+      const task = {
+        id: randomUUID(),
+        title: 'this is title',
+        description: 'this is description',
+        user: user,
+      };
       repositoryMock.findOne.mockReturnValue(task);
       const result = await service.getById(task.id, user);
       expect(result).toEqual(task);
     });
 
-    it('calls TasksService.getById should return the error ', async () => {
-      const user = { id: '123', username: 'Alni', password: '123', tasks: [] };
+    it('WHEN not found RETURN the error ', async () => {
+      const user = {
+        id: randomUUID(),
+        username: 'username',
+        password: 'password',
+        tasks: [],
+      };
+      const idParam = randomUUID();
       repositoryMock.findOne.mockReturnValue(null);
-      const result = service.getById('123', user);
+      const result = service.getById(idParam, user);
       expect(result).rejects.toThrow(
         new HttpException(
-          { code: 400001, message: `Task with ID[123] not found.` },
+          { code: 400001, message: `Task with ID[${idParam}] not found.` },
           HttpStatus.BAD_REQUEST,
         ),
       );
