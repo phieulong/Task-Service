7c719eed118af5397c02612f2366b7e276457d76
nhatnq3 <nhatnq3@onemount.com>
2023-02-20 12:05:33 +0700

    hehe

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { TasksController } from './tasks.controller';
 import { Task } from './tasks.entity';
 import { TasksService } from './tasks.service';
